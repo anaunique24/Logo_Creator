a4cdd3cf1236a4d769b625363936273d2a179683
Ana Grisham <anaunique@icloud.com>
2023-06-13 14:09:54 -0700

    created a shape file

--- index.js
+++ index.js
@@ -1,50 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { log } = require('console');
-// const Shapes = require('./Develop/examples');
+const { Circle, Square, Triangle} = require('./shapes');
 
-
-class Shape {
-    constructor(logoName, textColor, shapeColor){
-        this.logoName = logoName;
-        this.textColor = textColor;
-        this.shapeColor = shapeColor;
-    }
-    rendersvg(){
-        return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-${this.renderShape()}
-<text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.logoName}</text>
-
-        </svg>`
-    }
-    renderShape(){
-        return`<circle cx="150" cy="100" r="80" fill="${this.shapeColor}" />`
-    }
-}
-class Square extends Shape{
-    constructor(logoName, textColor, shapeColor) {
-    super(logoName, textColor, shapeColor)
-    }
-    renderShape(){
-        return`<rect x="90" y="40" width="120" height="120" fill="${this.shapeColor}" />`
-    }
-}
-class Circle extends Shape{
-    constructor(logoName, textColor, shapeColor) {
-    super(logoName, textColor, shapeColor)
-    }
-    renderShape(){
-        return`<circle cx="150" cy="100" r="80" fill="${this.shapeColor}" />`
-    }
-}
-class Triangle extends Shape{
-    constructor(logoName, textColor, shapeColor) {
-    super(logoName, textColor, shapeColor)
-    }
-    renderShape(){
-        return`<polygon points="150, 18 244, 182 56, 182" fill="${this.shapeColor}" />`
-    }
-}
 
 inquirer.prompt([
     {
