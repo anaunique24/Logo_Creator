e2577056e980465cb25bdf4e60a9c119e3ae43a2
Ana Grisham <anaunique@icloud.com>
2023-06-13 14:33:19 -0700

    cleaned code and created new logo

--- shapes/index.js
+++ shapes/index.js
@@ -8,8 +8,7 @@ class Shape {
         return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 ${this.render()}
 <text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.logoName}</text>
-
-        </svg>`
+</svg>`
     }
     setColor(shapeColor){
         this.shapeColor = shapeColor
